ff88a9476722d1557ac1b8c88c8d6f925217de1a
Aditi Vishwakarma <aditi07072000@gmail.com>
2021-12-14 10:20:32 +0530

    append contact names on the list

--- index.js
+++ index.js
@@ -5,17 +5,45 @@ const port = 8000;
 
 const app = express();
 
+contactList = [
+  {"Name": "Aditi" ,
+    "phone":"21203182"
+  },
+
+  {"Name": "Amisha" ,
+    "phone":"20392343182"
+  },
+
+  {"Name": "Sarita" ,
+    "phone":"294385345882"
+  }
+
+]
+
+
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+//parser data from form
+app.use(express.urlencoded());
 
 app.get('/', function(req,res){
-  return res.render('home', {title: "I am flying"});
+  return res.render('home', {
+    title: "Contact List",
+    contact_list:contactList
+  });
 });
 
 
 app.get('/practice' , function(req,res){
   return res.render('practice', 
   {title: "playing with ejs"});
+})
+
+app.post('/create-contact', (req, res) => {
+  
+  contactList.push(req.body,);
+   return res.redirect('/');
 })
 
 app.listen(port, function(err){
