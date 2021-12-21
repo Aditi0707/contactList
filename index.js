f8d38a178227f3329e0249177e29a3a149ed0b7a
Aditi Vishwakarma <aditi07072000@gmail.com>
2021-12-21 10:04:24 +0530

    add database

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 // const req = require('express/lib/request');
 const path = require('path');
 const port = 8000;
-
+const db = require('./config/mongoose');
 const app = express();
 
 app.set('view engine', 'ejs');
